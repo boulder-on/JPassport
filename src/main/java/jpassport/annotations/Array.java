9a6b645d3437533515eb55a5f9f3cde7af3e8a2d
boulder-on <duncan.mclean@gmail.com>
2021-05-15 06:44:38 -0400

    Adding the missing annotation

---
+++ src/main/java/jpassport/annotations/Array.java
@@ -0,0 +1,12 @@
+package jpassport.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.RECORD_COMPONENT, ElementType.FIELD})
+public @interface Array {
+    int length() default 1;
+}
